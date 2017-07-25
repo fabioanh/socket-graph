8bfc0e99d1cac4674af71b5114068addb01cd593
Fabio Navarrete <fabioanh@gmail.com>
2017-07-25 18:15:05 +0200

    Initial structure for project

---
+++ src/main/java/com/collibra/fabio/App.java
@@ -0,0 +1,13 @@
+package com.collibra.fabio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
